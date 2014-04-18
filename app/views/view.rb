a712d12f37f1c46edc90615fa9f78294e332fe3f
& <pair+alistairhill.lindseymenges@devbootcamp.com>
2014-04-18 15:44:07 -0700

    added card graphics

--- app/views/view.rb
+++ app/views/view.rb
@@ -23,10 +23,14 @@ class View
     puts ""
     puts "Welcome to the Command Line Casino!"
     puts "You're looking lucky - let's play some Blackjack!"
+    puts "--------------------------------"
+
   end
 
   def self.options
     puts "Would you like to hit or stand?"
+    puts "--------------------------------"
+
   end
 
   def self.view_cards(player_score, dealer_score)
@@ -34,10 +38,14 @@ class View
     puts "Your current score is: #{player_score}"
     puts "The dealer is showing: #{dealer_score}"
     puts "\n"
+    puts "--------------------------------"
+
   end
 
   def self.invalid_selection
     puts "That is not a valid selection. Please type 'hit' or 'stand'"
+    puts "--------------------------------"
+
   end
 
   def self.the_game_is_over(player_score, dealer_score)
@@ -46,6 +54,8 @@ class View
     puts "Your total score is: #{player_score}"
     puts "The dealer's total score is: #{dealer_score}"
     puts "\n"
+    puts "--------------------------------"
+
   end
 
   def self.player_wins
@@ -60,6 +70,8 @@ class View
     puts "I think you should keep playing and spend"
     puts "more money at the Command Line Casino."
     puts "Your luck will never run out!"
+    puts "--------------------------------"
+
   end
 
   def self.dealer_wins
@@ -72,13 +84,21 @@ class View
     puts "                        __/ |      "
     puts "                       |___/       "
     puts ""
-    puts "Sorry, looks like your luck ran out. Maybe spending more money at the Command Line Casino will bring it back! I'll go ahead and order you a martini."
+    puts "Sorry, looks like your luck ran out."
+    puts "Maybe spending more money at the"
+    puts "Command Line Casino will bring it back!"
+    puts "I'll go ahead and order you a martini."
+    puts "--------------------------------"
+
   end
 
   def self.push
     puts "Looks like there was a tie."
     puts "Some people would say that means everybody wins."
-    puts "You should keep your luck rolling and play another round at the Command Line Casino!"
+    puts "You should keep your luck rolling and play"
+    puts "another round at the Command Line Casino!"
+    puts "--------------------------------"
+
   end
 
   def self.again
@@ -89,6 +109,7 @@ class View
     puts "\n"
     puts "You currently have $#{money}"
     puts "\n"
+    puts "--------------------------------"
   end
 
   def self.bet
@@ -116,5 +137,79 @@ class View
     puts ""
     puts "We only love you when you have money."
     puts "And give me back that martini!"
+    puts "--------------------------------"
+  end
+end
+
+module CardString
+
+  def self.print(card_obj)
+    cards = {
+    "heart" => {
+    "Ace" =>".------.\n|A   _ |\n|( \\/ )|\n| \\  / |\n|  \\/ A|\n'------'",
+    "King" => ".------.\n|K   _ |\n|( \\/ )|\n| \\  / |\n|  \\/ K|\n'------'",
+    "Queen" => ".------.\n|Q   _ |\n|( \\/ )|\n| \\  / |\n|  \\/ Q|\n'------'",
+    "Jack" => ".------.\n|J   _ |\n|( \\/ )|\n| \\  / |\n|  \\/ J|\n'------'",
+    "10" => ".------.\n|10  _ |\n|( \\/ )|\n| \\  / |\n|  \\/10|\n'------'",
+    "9" => ".------.\n|9   _ |\n|( \\/ )|\n| \\  / |\n|  \\/ 9|\n'------'",
+    "8" => ".------.\n|8   _ |\n|( \\/ )|\n| \\  / |\n|  \\/ 8|\n'------'",
+    "7" => ".------.\n|7   _ |\n|( \\/ )|\n| \\  / |\n|  \\/ 7|\n'------'",
+    "6" => ".------.\n|6   _ |\n|( \\/ )|\n| \\  / |\n|  \\/ 6|\n'------'",
+    "5" => ".------.\n|5   _ |\n|( \\/ )|\n| \\  / |\n|  \\/ 5|\n'------'",
+    "4" => ".------.\n|4   _ |\n|( \\/ )|\n| \\  / |\n|  \\/ 4|\n'------'",
+    "3" => ".------.\n|3   _ |\n|( \\/ )|\n| \\  / |\n|  \\/ 3|\n'------'",
+    "2" => ".------.\n|2   _ |\n|( \\/ )|\n| \\  / |\n|  \\/ 2|\n'------'"
+    },
+    #diamonds
+    "diamond" => {
+    "Ace" => ".------.\n|A /\\  |\n| /  \\ |\n| \\  / |\n|  \\/ A|\n'------'",
+    "King" => ".------.\n|K /\\  |\n| /  \\ |\n| \\  / |\n|  \\/ K|\n'------'",
+    "Queen" => ".------.\n|Q /\\  |\n| /  \\ |\n| \\  / |\n|  \\/ Q|\n'------'",
+    "Jack" => ".------.\n|J /\\  |\n| /  \\ |\n| \\  / |\n|  \\/ J|\n'------'",
+    "10" => ".------.\n|10/\\  |\n| /  \\ |\n| \\  / |\n|  \\/10|\n'------'",
+    "9" => ".------.\n|9 /\\  |\n| /  \\ |\n| \\  / |\n|  \\/ 9|\n'------'",
+    "8" => ".------.\n|8 /\\  |\n| /  \\ |\n| \\  / |\n|  \\/ 8|\n'------'",
+    "7" => ".------.\n|7 /\\  |\n| /  \\ |\n| \\  / |\n|  \\/ 7|\n'------'",
+    "6" => ".------.\n|6 /\\  |\n| /  \\ |\n| \\  / |\n|  \\/ 6|\n'------'",
+    "5" => ".------.\n|5 /\\  |\n| /  \\ |\n| \\  / |\n|  \\/ 5|\n'------'",
+    "4" => ".------.\n|4 /\\  |\n| /  \\ |\n| \\  / |\n|  \\/ 4|\n'------'",
+    "3" => ".------.\n|3 /\\  |\n| /  \\ |\n| \\  / |\n|  \\/ 3|\n'------'",
+    "2" => ".------.\n|2 /\\  |\n| /  \\ |\n| \\  / |\n|  \\/ 2|\n'------'"
+    },
+    #spades
+    "spade" => {
+    "Ace" => ".------.\n|A .   |\n; / \\  |\n|(_,_) |\n|  I  A|\n'------'",
+    "King" => ".------.\n|K .   |\n; / \\  |\n|(_,_) |\n|  I  K|\n'------'",
+    "Queen" => ".------.\n|Q .   |\n; / \\  |\n|(_,_) |\n|  I  Q|\n'------'",
+    "Jack"=> ".------.\n|J .   |\n; / \\  |\n|(_,_) |\n|  I  J|\n'------'",
+    "10" => ".------.\n|10.   |\n; / \\  |\n|(_,_) |\n|  I 10|\n'------'",
+    "9" => ".------.\n|9 .   |\n; / \\  |\n|(_,_) |\n|  I  9|\n'------'",
+    "8" => ".------.\n|8 .   |\n; / \\  |\n|(_,_) |\n|  I  8|\n'------'",
+    "7" => ".------.\n|7 .   |\n; / \\  |\n|(_,_) |\n|  I  7|\n'------'",
+    "6" => ".------.\n|6 .   |\n; / \\  |\n|(_,_) |\n|  I  6|\n'------'",
+    "5" => ".------.\n|5 .   |\n; / \\  |\n|(_,_) |\n|  I  5|\n'------'",
+    "4" => ".------.\n|4 .   |\n; / \\  |\n|(_,_) |\n|  I  4|\n'------'",
+    "3" => ".------.\n|3 .   |\n; / \\  |\n|(_,_) |\n|  I  3|\n'------'",
+    "2" => ".------.\n|2 .   |\n; / \\  |\n|(_,_) |\n|  I  2|\n'------'"
+    },
+    #clubs
+    "club" => {
+    "Ace" => ".------.\n|A _   |\n| ( )  |\n|(_x_) |\n|  Y  A|\n'------'",
+    "King" => ".------.\n|K _   |\n| ( )  |\n|(_x_) |\n|  Y  K|\n'------'",
+    "Queen" => ".------.\n|Q _   |\n| ( )  |\n|(_x_) |\n|  Y  Q|\n'------'",
+    "Jack" => ".------.\n|J _   |\n| ( )  |\n|(_x_) |\n|  Y  J|\n'------'",
+    "10" => ".------.\n|10_   |\n| ( )  |\n|(_x_) |\n|  Y 10|\n'------'",
+    "9" => ".------.\n|9 _   |\n| ( )  |\n|(_x_) |\n|  Y  9|\n'------'",
+    "8" => ".------.\n|8 _   |\n| ( )  |\n|(_x_) |\n|  Y  8|\n'------'",
+    "7" => ".------.\n|7 _   |\n| ( )  |\n|(_x_) |\n|  Y  7|\n'------'",
+    "6" => ".------.\n|6 _   |\n| ( )  |\n|(_x_) |\n|  Y  6|\n'------'",
+    "5" => ".------.\n|5 _   |\n| ( )  |\n|(_x_) |\n|  Y  5|\n'------'",
+    "4" => ".------.\n|4 _   |\n| ( )  |\n|(_x_) |\n|  Y  4|\n'------'",
+    "3" => ".------.\n|3 _   |\n| ( )  |\n|(_x_) |\n|  Y  3|\n'------'",
+    "2" => ".------.\n|2 _   |\n| ( )  |\n|(_x_) |\n|  Y  2|\n'------'"
+    }
+    }
+    puts cards[card_obj.suit][card_obj.display_value]
+    puts "--------------------------------"
   end
 end
